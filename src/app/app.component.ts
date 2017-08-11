f591ae845b1fe986dc0fcdba7aa15a56ccf1f0aa
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-08-11 15:24:20 +0000

    Using two way bindind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,15 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
 
+  // You can use ngModel in 3 forms:
+  // 1- No binding (ex: just use ngModel, tell angular to control the input value)
+  // 2- One way binding (ex: [ngModel] and defaultQuestion )
+  // 3- Two way binding (ex: [(ngModel)] and answer)
+
   // It's is useful if you need to access the form early than submitted
   @ViewChild('f') form: NgForm;
-
   defaultQuestion = 'pet';
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
