74067ebaecb5cb9d4963289fe00f88cfdfa10797
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-08-11 11:44:38 +0000

    Accessing the Form with @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -7,11 +7,19 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // It's is useful if you need to access the form early than submitted
+  @ViewChild('f') form: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm) {
+/*  onSubmit(form: NgForm) {
     console.log(form);
+  }*/
+
+  onSubmit() {
+    console.log(this.form);
   }
 }
